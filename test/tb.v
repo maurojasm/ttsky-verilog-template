537b14147fd2e42bd90913b2284ba13525ea7ece
Mauricio Rojas <79242002+maurojasm@users.noreply.github.com>
2026-03-03 22:27:39 -0800

    added assertion if statements

--- test/tb.v
+++ test/tb.v
@@ -27,7 +27,61 @@ module tb ();
   wire VGND = 1'b0;
 `endif
 
+  // reference module
+  reg [7:0] B;
+  reg [8:0] temp_result;
+  reg [7:0] expected_out;
+  reg [0:0] expected_carry;
+
+  B = {3'b0, uio_in[7:3]};
+
+  always @(posedge clk) begin
+    if(!rst_n) begin
+      expected_out <= 8'b0;
+      expected_carry <= 1'b0;
+    end
+    else begin
+      case (uio_in[2:0])
+        3'b000: begin // add
+          temp_result = {1'b0,ui_in} + {1'b0,B};
+          expected_out = temp_result[7:0];
+          expected_carry = temp_result[8];
+        end
+        3'b001: begin // sub
+          temp_result = {1'b0,ui_in} - {1'b0,B};
+          expected_out = temp_result[7:0];
+          expected_carry = temp_result[8];
+        end
+        3'b010: begin // not A
+          expected_out = ~ui_in;
+          expected_carry = 0;
+        end
+        3'b011: begin // and
+          expected_out = ui_in & B;
+          expected_carry = 0;
+        end
+        3'b100: begin // or
+          expected_out = ui_in | B;
+          expected_carry = 0;
+        end
+        3'b101: begin // xor
+          expected_out = ui_in ^ B;
+          expected_carry = 0;
+        end
+        3'b110: begin // sll
+          expected_out = {ui_in[6:0], 1'b0};
+          expected_carry = 0;
+        end
+        3'b111: begin // pass A
+          expected_out = ui_in;
+          expected_carry = 0;
+        end
+      endcase
+    end
+  end
+
   // Replace tt_um_example with your module name:
+  // dut
   tt_um_marojasm_alu user_project (
 
       // Include power ports for the Gate Level test:
@@ -56,49 +110,114 @@ module tb ();
     uio_in = 0;
 
     #500
-    // add
     rst_n = 1;
+    // add
     ui_in = 8'd15;
     uio_in = {5'd7, 3'b000};
+    if (uo_out !== expected_out) begin
+        $display("ERROR: opcode=%b A=%d B=%d Expected=%d Got=%d", opcode, A, B, expected_out, uo_out);
+        $fatal;
+      end
+      if (uio_out[7] !== expected_carry) begin
+        $display("ERROR: Carry mismatch opcode=%b A=%d B=%d Expected carry=%b Got carry=%b", opcode, A, B, expected_carry, uio_out[7]);
+        $fatal;
+      end
 
     #200
     // sub
     ui_in = 8'd20;
     uio_in = {5'd3, 3'b001};
+    if (uo_out !== expected_out) begin
+        $display("ERROR: opcode=%b A=%d B=%d Expected=%d Got=%d", opcode, A, B, expected_out, uo_out);
+        $fatal;
+      end
+      if (uio_out[7] !== expected_carry) begin
+        $display("ERROR: Carry mismatch opcode=%b A=%d B=%d Expected carry=%b Got carry=%b", opcode, A, B, expected_carry, uio_out[7]);
+        $fatal;
+      end
 
     #200
     // not A
     ui_in = 8'b11110000;
     uio_in = {5'd0, 3'b010};
+    if (uo_out !== expected_out) begin
+        $display("ERROR: opcode=%b A=%d B=%d Expected=%d Got=%d", opcode, A, B, expected_out, uo_out);
+        $fatal;
+      end
+      if (uio_out[7] !== expected_carry) begin
+        $display("ERROR: Carry mismatch opcode=%b A=%d B=%d Expected carry=%b Got carry=%b", opcode, A, B, expected_carry, uio_out[7]);
+        $fatal;
+      end
 
     #200
     // and
     ui_in = 8'b10101010;
     uio_in = {5'b11111, 3'b011};
+    if (uo_out !== expected_out) begin
+        $display("ERROR: opcode=%b A=%d B=%d Expected=%d Got=%d", opcode, A, B, expected_out, uo_out);
+        $fatal;
+      end
+      if (uio_out[7] !== expected_carry) begin
+        $display("ERROR: Carry mismatch opcode=%b A=%d B=%d Expected carry=%b Got carry=%b", opcode, A, B, expected_carry, uio_out[7]);
+        $fatal;
+      end
 
     #200
     // or
     ui_in = 8'd20;
     uio_in = {5'd3, 3'b100};
+    if (uo_out !== expected_out) begin
+        $display("ERROR: opcode=%b A=%d B=%d Expected=%d Got=%d", opcode, A, B, expected_out, uo_out);
+        $fatal;
+      end
+      if (uio_out[7] !== expected_carry) begin
+        $display("ERROR: Carry mismatch opcode=%b A=%d B=%d Expected carry=%b Got carry=%b", opcode, A, B, expected_carry, uio_out[7]);
+        $fatal;
+      end
 
     #200
     // xor
     ui_in = 8'd20;
     uio_in = {5'd3, 3'b101};
+    if (uo_out !== expected_out) begin
+        $display("ERROR: opcode=%b A=%d B=%d Expected=%d Got=%d", opcode, A, B, expected_out, uo_out);
+        $fatal;
+      end
+      if (uio_out[7] !== expected_carry) begin
+        $display("ERROR: Carry mismatch opcode=%b A=%d B=%d Expected carry=%b Got carry=%b", opcode, A, B, expected_carry, uio_out[7]);
+        $fatal;
+      end
 
     #200
     // sll
     ui_in = 8'd20;
     uio_in = {5'd0, 3'b110};
+    if (uo_out !== expected_out) begin
+        $display("ERROR: opcode=%b A=%d B=%d Expected=%d Got=%d", opcode, A, B, expected_out, uo_out);
+        $fatal;
+      end
+      if (uio_out[7] !== expected_carry) begin
+        $display("ERROR: Carry mismatch opcode=%b A=%d B=%d Expected carry=%b Got carry=%b", opcode, A, B, expected_carry, uio_out[7]);
+        $fatal;
+      end
 
     #200
     // pass
     ui_in = 8'd20;
     uio_in = {5'd0, 3'b111};
+    if (uo_out !== expected_out) begin
+        $display("ERROR: opcode=%b A=%d B=%d Expected=%d Got=%d", opcode, A, B, expected_out, uo_out);
+        $fatal;
+      end
+      if (uio_out[7] !== expected_carry) begin
+        $display("ERROR: Carry mismatch opcode=%b A=%d B=%d Expected carry=%b Got carry=%b", opcode, A, B, expected_carry, uio_out[7]);
+        $fatal;
+      end
 
     #200
     ui_in = 8'd0;
     uio_in = {5'd0, 3'b000};
 
+    $finish;
   end
 endmodule
